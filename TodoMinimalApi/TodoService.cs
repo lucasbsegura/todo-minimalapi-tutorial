72e733ce940ad5413e2e98dfff660ecfc1b1c954
Lucas Segura <lucasbsegura@gmail.com>
2023-07-02 03:26:35 -0300

    creating TodoService

---
+++ TodoMinimalApi/TodoService.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoMinimalApi
+{
+    public static class TodoService
+    {
+
+        public static async Task<IResult> GetAllTodos(TodoDb db)
+        {
+            return TypedResults.Ok(await db.Todos.Select(x => new TodoItemDTO(x)).ToArrayAsync());
+        }
+
+        public static async Task<IResult> GetCompleteTodos(TodoDb db)
+        {
+            return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).Select(x => new TodoItemDTO(x)).ToListAsync());
+        }
+
+        public static async Task<IResult> GetTodo(int id, TodoDb db)
+        {
+            return await db.Todos.FindAsync(id)
+                is Todo todo
+                    ? TypedResults.Ok(new TodoItemDTO(todo))
+                    : TypedResults.NotFound();
+        }
+
+        public static async Task<IResult> CreateTodo(TodoItemDTO todoItemDTO, TodoDb db)
+        {
+            var todoItem = new Todo
+            {
+                IsComplete = todoItemDTO.IsComplete,
+                Name = todoItemDTO.Name
+            };
+
+            db.Todos.Add(todoItem);
+            await db.SaveChangesAsync();
+
+            todoItemDTO = new TodoItemDTO(todoItem);
+
+            return TypedResults.Created($"/todoitems/{todoItem.Id}", todoItemDTO);
+        }
+
+        public static async Task<IResult> UpdateTodo(int id, TodoItemDTO todoItemDTO, TodoDb db)
+        {
+            var todo = await db.Todos.FindAsync(id);
+
+            if (todo is null) return TypedResults.NotFound();
+
+            todo.Name = todoItemDTO.Name;
+            todo.IsComplete = todoItemDTO.IsComplete;
+
+            await db.SaveChangesAsync();
+
+            return TypedResults.NoContent();
+        }
+
+        public static async Task<IResult> DeleteTodo(int id, TodoDb db)
+        {
+            if (await db.Todos.FindAsync(id) is Todo todo)
+            {
+                db.Todos.Remove(todo);
+                await db.SaveChangesAsync();
+
+                TodoItemDTO todoItemDTO = new TodoItemDTO(todo);
+
+                return TypedResults.Ok(todoItemDTO);
+            }
+
+            return TypedResults.NotFound();
+        }
+    }
+}
