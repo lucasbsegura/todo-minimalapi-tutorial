70895f1d7b29487efdda1bf9e1d3f04615489ad5
Lucas Segura <lucasbsegura@gmail.com>
2023-07-02 03:51:37 -0300

    refactor

--- TodoMinimalApi/Program.cs
+++ TodoMinimalApi/Program.cs
@@ -20,11 +20,23 @@ if (app.Environment.IsDevelopment())
 
 RouteGroupBuilder todoItems = app.MapGroup("/todoitems");
 
-todoItems.MapGet("/", TodoService.GetAllTodos).WithName("AllTodos").WithOpenApi();
-todoItems.MapGet("/complete", TodoService.GetCompleteTodos).WithName("CompleteTodos").WithOpenApi();
-todoItems.MapGet("/{id}", TodoService.GetTodo).WithName("Todo").WithOpenApi();
-todoItems.MapPost("/", TodoService.CreateTodo).WithName("CreateTodo").WithOpenApi();
-todoItems.MapPut("/{id}", TodoService.UpdateTodo).WithName("UpdateTodo").WithOpenApi();
-todoItems.MapDelete("/{id}", TodoService.DeleteTodo).WithName("DeleteTodo").WithOpenApi();
+todoItems.MapGet("/", TodoService.GetAllTodos)
+    .WithName("AllTodos")
+    .WithOpenApi();
+todoItems.MapGet("/complete", TodoService.GetCompleteTodos)
+    .WithName("CompleteTodos")
+    .WithOpenApi();
+todoItems.MapGet("/{id}", TodoService.GetTodo)
+    .WithName("Todo")
+    .WithOpenApi();
+todoItems.MapPost("/", TodoService.CreateTodo)
+    .WithName("CreateTodo")
+    .WithOpenApi();
+todoItems.MapPut("/{id}", TodoService.UpdateTodo)
+    .WithName("UpdateTodo")
+    .WithOpenApi();
+todoItems.MapDelete("/{id}", TodoService.DeleteTodo)
+    .WithName("DeleteTodo")
+    .WithOpenApi();
 
 app.Run();
